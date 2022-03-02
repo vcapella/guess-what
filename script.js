da56f90d303dd03f6164fb95d0dedb04fff8862d
Victor Capella <capella89@outlook.com>
2022-03-02 02:11:39 -0500

    update interaction functions

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const targetWords = ["thing", "those", "cigar", "smoke", "sleep"];
 const dictionary = ["thing", "those", "cigar", "smoke", "sleep"];
 
+const WORLD_LENGTH = 5;
+const guessGrid = document.querySelector("[data-guess-grid]");
+const offsetFromDate = new Date(2022, 0, 1);
+const msOffset = Date.now() - offsetFromDate;
+const dayOffset = msOffset / 1000 / 60 / 60 / 24;
+const targetWord = targetWords[Math.floor(dayOffset)];
+
+startInteraction();
+
 function startInteraction() {
   document.addEventListener("click", handleMouseClick);
   document.addEventListener("keydown", handleKeyPress);
@@ -43,4 +52,28 @@ function handleKeyPress(e) {
     pressKey(e.key);
     return;
   }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORLD_LENGTH) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+  if (lastTile == null) return;
+  lastTile.textContent = "";
+  delete lastTile.dataset.state;
+  delete lastTile.dataset.letter;
+}
+
+function submitGuess() {}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]');
 }
